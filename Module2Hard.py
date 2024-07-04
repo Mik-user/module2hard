98ced3f21b234e0c2bc185964e443802320a9e09
Mik-user <mikmikh@yandex.ru>
2024-07-04 12:54:42 +0300

    Add files via upload

--- Module2Hard.py
+++ Module2Hard.py
@@ -1,27 +1,20 @@
-def make_parole (n): # n - значение 1-ого камня
-    list_ = list() # список кратных делителей n
-    list_.append(n)
-    for i in range(n): # составление списка кратных делителей n
-        if n % (i + 1) == 0 and (i + 1) > 2 and (i + 1) != n:
-            list_.append(i + 1)
-    pary = list()# список слагаемых для списка list_
-    for index in list_:
-        for i in range(index):
-            j = index - i - 1
-            if i + 1 < j:
-                summ = str(i + 1) + str(j)
-                pary.append(summ)
-    pary.sort()
-    parole = str()
-    for i in pary:
-        parole = parole + i
-    return parole
-first_stone = list()
-stone_min = 3
-stone_max = input( 'Максимальное значение первого камня (по условию - 20):')
-stone = int (stone_min)
-while stone <= int (stone_max):#формирование "базы" первых камней
-    first_stone.append(stone)
-    stone = stone +1
-for i in first_stone:
-    print ('Для', i, 'Пароль:', make_parole(i))
+def make_parole(a):
+    for L in range(1, a+1):
+        Parole = str()
+        for i in range(1, L):
+         for j in range(i+1, L):
+            if L % (i +j) == 0:
+             Parole = Parole + str(i) + str(j)
+    print(f'Пароль для {a}: {Parole}')
+while 1>0:
+    x = (input ('Значение первого камня (от 3 до 20):'))
+    if x.isnumeric():
+        n_stone = int(x)
+        if n_stone>= 3 and n_stone<=20:
+            make_parole(n_stone)
+            break
+        else:
+            print("Некорректное значение. Попробуйте снова")
+    else:
+        print('Используйте, пожалйста, цифры')
+        continue
